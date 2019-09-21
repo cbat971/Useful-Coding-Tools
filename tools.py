ab37af1fcb79e68cc67dfb5b5c40aef01f76c3af
cbat971 <40727936+cbat971@users.noreply.github.com>
2019-09-21 17:23:14 -0400

    Add files via upload
    I've only added one tool but plan to add more. I've made it so I can set things to info in my scripts I'm working in case I haven't commented well, I'm made this while trying to go back and add comments to mycode.

---
+++ tools.py
@@ -0,0 +1,7 @@
+def check(info):
+	print("###   Debug and notes   ###")
+	print("## The type this info is: ")
+	print(str(type(info)))
+	print("##the information set as info is:")
+	print(str((info)))
+	print("###   End Debug   ###")
